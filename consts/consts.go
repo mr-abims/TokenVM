87832907730bd15e62be8a6ff7d530b20efe55e5
mr-abims <adebayoabimbola9@gmail.com>
2024-01-27 19:40:23 +0100

    config && Registry

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "ABIMS"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "ABIMSTN"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "ATN"
 )
 
 var ID ids.ID
